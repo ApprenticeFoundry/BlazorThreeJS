44763c06649de473c082d10496caba843b653c75
Steve <stephen.r.strong@saic.com>
2024-11-06 21:36:43 -0500

    feat: Refactor Material and Mesh3D classes for improved structure and readability; update namespaces and formatting

--- Core/Material.cs
+++ Core/Material.cs
@@ -6,55 +6,55 @@ using FoundryRulesAndUnits.Models;
 
 
 
-namespace BlazorThreeJS.Materials
+namespace BlazorThreeJS.Materials;
+
+[JsonDerivedType(typeof(MeshStandardMaterial))]
+public abstract class Material : ITreeNode
 {
-    [JsonDerivedType(typeof(MeshStandardMaterial))]
-    public abstract class Material : ITreeNode
+    protected StatusBitArray StatusBits = new();
+
+    protected Material(string type) => this.Type = type;
+
+    public string Type { get; } = nameof(Material);
+
+    public string Name { get; set; } = string.Empty;
+
+    public Guid Uuid { get; set; } = Guid.NewGuid();
+
+    public virtual string GetTreeNodeTitle()
     {
-        protected StatusBitArray StatusBits = new();
+        return $"{Name} [{Uuid}] => {Type} ({GetType().Name})";
+    }
 
-        protected Material(string type) => this.Type = type;
+    public IEnumerable<TreeNodeAction>? GetTreeNodeActions()
+    {
+        return null;
+    }
+    public bool GetIsExpanded()
+    {
+        return this.StatusBits.IsExpanded;
+    }
 
-        public string Type { get; } = nameof(Material);
+    public bool SetExpanded(bool value)
+    {
+        this.StatusBits.IsExpanded = value;
+        return value;
+    }
 
-        public string Name { get; set; } = string.Empty;
+    public bool GetIsSelected()
+    {
+        return this.StatusBits.IsSelected;
+    }
 
-        public Guid Uuid { get; set; } = Guid.NewGuid();
+    public bool SetSelected(bool value)
+    {
+        this.StatusBits.IsSelected = value;
+        return value;
+    }
 
-        public virtual string GetTreeNodeTitle()
-        {
-            return $"{Name} [{Uuid}] => {Type} ({GetType().Name})";
-        }
-
-        public IEnumerable<TreeNodeAction>? GetTreeNodeActions()
-        {
-            return null;
-        }
-        public bool GetIsExpanded()
-        {
-            return this.StatusBits.IsExpanded;
-        }
-
-        public bool SetExpanded(bool value)
-        {
-            this.StatusBits.IsExpanded = value;
-            return value;
-        }
-
-        public bool GetIsSelected()
-        {
-            return this.StatusBits.IsSelected;
-        }
-
-        public bool SetSelected(bool value)
-        {
-            this.StatusBits.IsSelected = value;
-            return value;
-        }
-
-        public IEnumerable<ITreeNode> GetTreeChildren()
-        {
-            return Enumerable.Empty<ITreeNode>();
-        }
+    public IEnumerable<ITreeNode> GetTreeChildren()
+    {
+        return Enumerable.Empty<ITreeNode>();
     }
 }
+
--- Core/Mesh3D.cs
+++ Core/Mesh3D.cs
@@ -1,26 +1,20 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: Blazor3D.Objects.Mesh
-// Assembly: Blazor3D, Version=0.1.24.0, Culture=neutral, PublicKeyToken=null
-// MVID: 8589B0D0-D62F-4099-9D8A-332F65D16B15
-// Assembly location: Blazor3D.dll
-
-using BlazorThreeJS.Core;
+﻿using BlazorThreeJS.Core;
 using BlazorThreeJS.Geometires;
 using BlazorThreeJS.Materials;
 
 
 
-namespace BlazorThreeJS.Objects
-{
-    public class Mesh3D : Object3D
-    {
-        public Mesh3D()
-          : base(nameof(Mesh3D))
-        {
-        }
+namespace BlazorThreeJS.Objects;
 
-        public Material Material { get; set; } = (Material)new MeshStandardMaterial();
+  public class Mesh3D : Object3D
+  {
+      public Mesh3D()
+        : base(nameof(Mesh3D))
+      {
+      }
 
-        public BufferGeometry Geometry { get; set; } = (BufferGeometry)new BoxGeometry();
-    }
-}
+      public Material Material { get; set; } = (Material)new MeshStandardMaterial();
+
+      public BufferGeometry Geometry { get; set; } = (BufferGeometry)new BoxGeometry();
+  }
+
