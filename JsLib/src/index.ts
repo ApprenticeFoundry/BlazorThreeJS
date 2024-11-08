04fd33c4ff26ced6ae45f48a3008deac20dbecf0
Steve <stephen.r.strong@saic.com>
2024-11-08 08:33:35 -0500

    feat: Simplify Load function by removing console logs and directly establishing Viewer3D

--- JsLib/src/index.ts
+++ JsLib/src/index.ts
@@ -1,15 +1,14 @@
 
 import { ViewManager } from './Viewer/ViewManager';
 
-namespace JSInterop {
+namespace JSInterop 
+{
     export function Load(): void {
         console.log('In Load JSInterop');
         var manager = new ViewManager();
         window['ViewManager'] = manager;
-        var original = manager.establishViewer3D('BlazorThreeJS');
+        manager.establishViewer3D('BlazorThreeJS');
 
-
-        console.log('original', original);
 
         //window['BlazorThreeJS'] = new Viewer3D();
     }
